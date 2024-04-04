5b2cb2829eece21b5bec6fa41f7a6744fe862ce7
Dhenzel Lima Ribeiro <dhenzellimaribeiro@gmail.com>
2024-04-04 11:38:57 -0300

    FEAT: switching from functional components to class components

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Welcome to calculator</Text>
+        <Text>App.js</Text>
+      </View>
+    );
+  }
+}
