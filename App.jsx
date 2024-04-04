ef76bbfe6b88396a8fb59b0862b813d60b035d0e
Dhenzel Lima Ribeiro <dhenzellimaribeiro@gmail.com>
2024-04-04 12:39:59 -0300

    NEW: Buttons Component

--- App.jsx
+++ App.jsx
@@ -1,13 +1,41 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
+import Button from './src/components/Button';
 
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Text>Welcome to calculator</Text>
-        <Text>App.js</Text>
+      <View style={styles.container}>
+        <View style={styles.buttons}>
+          <Button label="AC" />
+          <Button label="/" />
+          <Button label="7" />
+          <Button label="8" />
+          <Button label="9" />
+          <Button label="*" />
+          <Button label="4" />
+          <Button label="5" />
+          <Button label="6" />
+          <Button label="-" />
+          <Button label="1" />
+          <Button label="2" />
+          <Button label="3" />
+          <Button label="+" />
+          <Button label="0" />
+          <Button label="." />
+          <Button label="=" />
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+});
