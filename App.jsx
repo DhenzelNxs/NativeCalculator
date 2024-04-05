c622162d5117a72acb4403fb8f095b23fa30b27e
Dhenzel Lima Ribeiro <dhenzellimaribeiro@gmail.com>
2024-04-05 12:35:32 -0300

    FEAT: adding all operations

--- App.jsx
+++ App.jsx
@@ -37,7 +37,28 @@ export default class App extends Component {
     this.setState({...initialState});
   };
 
-  setOperation = operation => {};
+  setOperation = operation => {
+    if (this.state.current === 0) {
+      this.setState({operation, current: 1, clearDisplay: true});
+    } else {
+      const equals = operation === '=';
+      const values = [...this.state.values];
+      try {
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`);
+      } catch (e) {
+        values[0] = this.state.values[0];
+      }
+
+      values[1] = 0;
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: true,
+        values,
+      });
+    }
+  };
 
   render() {
     const {displayValue} = this.state;
