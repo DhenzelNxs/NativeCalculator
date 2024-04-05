dab35b8107aaf52b43d884fd8d314e119d17e6bc
Dhenzel Lima Ribeiro <dhenzellimaribeiro@gmail.com>
2024-04-05 12:20:11 -0300

    FEAT: numerical logic bug resolved

--- App.jsx
+++ App.jsx
@@ -3,18 +3,38 @@ import {Text, View, StyleSheet} from 'react-native';
 import Button from './src/components/Button';
 import Display from './src/components/Display';
 
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+};
+
 export default class App extends Component {
-  state = {
-    displayValue: '0',
-  };
+  state = {...initialState};
 
   addDigit = n => {
-    const {displayValue} = this.state;
-    this.setState({displayValue: n});
+    if (n === '.' && this.state.displayValue.includes('.')) {
+      return;
+    }
+
+    const clearDisplay =
+      this.state.displayValue === '0' || this.state.clearDisplay;
+    const currentValue = clearDisplay ? '' : this.state.displayValue;
+    const displayValue = currentValue + n;
+    this.setState({displayValue, clearDisplay: false});
+
+    if (n !== '.') {
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+      values[this.state.current] = newValue;
+      this.setState({values});
+    }
   };
 
   clearMemory = () => {
-    this.setState({displayValue: '0'});
+    this.setState({...initialState});
   };
 
   setOperation = operation => {};
